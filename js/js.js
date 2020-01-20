186ef683be6038f0c16d8c0bb459f51b2e885d36
David Kljestan <dvdkljestan@gmail.com>
2020-01-20 22:22:03 +0100

    all changes

---
+++ js/js.js
@@ -0,0 +1,35 @@
+document.querySelector(".icon").addEventListener("click",function(e){
+	e.preventDefault();
+	document.querySelector(".navbar").classList.toggle("isOpen");	
+});
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
